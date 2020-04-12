16c740288cc0f8ebf798c7bc3cac533d1b3b2491
ivf <ivyfan@MacBook-Pro-8.local>
2020-04-12 14:02:16 -0400

    split into MVC; add resultsViewController and tipBrain.swift to model

---
+++ Tipsy/Controller/resultsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  resultsViewController.swift
+//  Tipsy
+//
+//  Created by Ivy Victoria Fan on 4/12/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class resultsViewController: UIViewController {
+
+}
---
+++ Tipsy/Model/tipBrain.swift
@@ -0,0 +1,9 @@
+//
+//  tipBrain.swift
+//  Tipsy
+//
+//  Created by Ivy Victoria Fan on 4/12/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
